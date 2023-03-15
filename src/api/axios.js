47ec34be14fabc16459336d44f5a478179a05671
Prashanth <sgprashanth.raju@gmail.com>
2023-03-15 22:59:16 +0530

    URL updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://emp-backend.onrender.com';
+const BASE_URL = 'https://https-emp-backend-onrender-com.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
